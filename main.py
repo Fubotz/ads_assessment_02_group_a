d540ada74ed45620f9be00c1148e0c95fda9202d
Fubotz <fabian.schmbck@gmail.com>
2024-04-30 11:11:50 +0200

    Fabian_Schambeck_Assessment_2_Group_A

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 """
 Simple Calculator
 """
-# Provide your solution here
+# Provide your solution here:
 
+
+# def calculate(a: int, b: int, c: str):
+#     if c == "+":
+#         print(f"The solution is {a + b}")
+#     elif c == "-":
+#         print(f"The solution is {a - b}")
+#     elif c == "*":
+#         print(f"The solution is {a * b}")
+#     elif c == "/":
+#         if b == 0:
+#             print("Division by 0 is not allowed!")
+#         else:
+#             print(f"The solution is {a / b}")
+#     elif c != "+" or "-" or "*" or "/":
+#         print("Invalid Operator")
+#
+#
+# calculate(a=12, b=8, c="/")     # change input here to calculate
 
 """
 Reverse Word
 """
-# Provide your solution here
+# Provide your solution here:
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
+
+# reverse_word = str(input("Type in a string: "))
+#
+# for i in range(len(reverse_word) - 1, -1, -1):
+#     print(end=reverse_word[i].upper())
 
